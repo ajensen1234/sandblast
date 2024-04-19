86614db794d9c3334a85ddab82c6ea6ea31370ae
Andrew Jensen <contact@ajensen.org>
2024-04-18 21:22:22 -0400

    Some initial shader inits

--- src/buffer.rs
+++ src/buffer.rs
@@ -7,6 +7,10 @@ pub struct GpuBuffer<T> {
     size: usize,
     _marker: PhantomData<T>,
 }
+// TODO: Better error handling (Pretty sure we should be using the `anywho/how(?)` crate for this)
+// TODO: Make sure that the generic types passed in are good
+// TODO: Validate all the assumptions about the permissions that we give.
+// permissions might actually be something that some users might want control over. See if we can overload `new` to make it super permissive with no usage passed in, but fully permissive with nothing passed on
 
 impl<T: bytemuck::Pod + bytemuck::Zeroable> GpuBuffer<T> {
     /// This will create a sandblast structure called a `GpuBuffer`
@@ -32,7 +36,7 @@ impl<T: bytemuck::Pod + bytemuck::Zeroable> GpuBuffer<T> {
         return Self {
             buffer,
             size: data.len() * std::mem::size_of::<T>(), // TODO: Check if we need to change this to the bytemuck representation
-            _marker: PhantomData,
+            _marker: PhantomData, //TODO: Make sure that this isnt going to mess things up  sdf
         };
     }
 }
--- src/shader.rs
+++ src/shader.rs
@@ -1,1 +1,26 @@
+use std::fs::{self, File };
+use std::io::Read;
+use std::marker::PhantomData;
+use std::path::PathBuf;
+use std::str;
+use crate::buffer::GpuBuffer;
 
+//TODO: Figure out which things actually need to live in the compute shader vs the things that just need to live as implementations
+// This might just need to be a function instead of a struct
+// TODO: Remove unwraps and eventually return error codes based on waht we are expecting to pass through
+pub struct ComputeShader<T> {
+    _marker: PhantomData<T>
+}
+
+
+impl<T> ComputeShader<T>{
+    pub async fn run(device: wgpu::Device, wgsl_file: String, buffers: &[GpuBuffer<T>], dispatch_groups: (usize, usize, usize)) {
+        let mut file = File::open(wgsl_file).unwrap();
+        let mut wgsl_code = String::new();
+        file.read_to_string(&mut wgsl_code).unwrap();
+        let cs_mod = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+            label: None,
+            source: wgpu::ShaderSource::Wgsl(wgsl_code.into()),
+        });
+    }
+}
