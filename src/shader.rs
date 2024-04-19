f595ecfddbbd63b8035265d764095b9ad093eca9
Andrew Jensen <contact@ajensen.org>
2024-04-18 22:15:57 -0400

    Updates

--- src/shader.rs
+++ src/shader.rs
@@ -1,8 +1,6 @@
-use std::fs::{self, File };
+use std::fs:: File ;
 use std::io::Read;
 use std::marker::PhantomData;
-use std::path::PathBuf;
-use std::str;
 use crate::buffer::GpuBuffer;
 
 //TODO: Figure out which things actually need to live in the compute shader vs the things that just need to live as implementations
@@ -14,6 +12,12 @@ pub struct ComputeShader<T> {
 
 
 impl<T> ComputeShader<T>{
+    /// This is the code that will house all of the necessary functions to
+    /// run a compute shader without any of the necessary boilerplate.
+    ///
+    /// You should simply be able to call `run()` while passing the correct files, buffers, and devices
+    /// into the protocol, and everything boilerplate will be abstracted away.
+    /// Voila
     pub async fn run(device: wgpu::Device, wgsl_file: String, buffers: &[GpuBuffer<T>], dispatch_groups: (usize, usize, usize)) {
         let mut file = File::open(wgsl_file).unwrap();
         let mut wgsl_code = String::new();
