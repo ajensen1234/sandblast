549cec79f8d17f5e4c115722c358c6a982153c41
l <>
2024-04-22 17:33:33 -0400

    Remove commented code from testing

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,3 @@ pub mod buffer;
 pub mod shader;
 pub mod device;
 pub mod matrix_serialization_utils;
-// pub mod gpu_matrix;
--- src/matrix_serialization_utils.rs
+++ src/matrix_serialization_utils.rs
@@ -1,6 +1,5 @@
 use bytemuck::{Pod, cast_slice};
 use nalgebra::DMatrix;
-// use crate::gpu_matrix::GPUMatrix;
 
 pub fn matrix_to_casted_array<U, T>(matrix: &DMatrix<U>) -> &[T]
 where
